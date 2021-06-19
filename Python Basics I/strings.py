03c0a6240f8b11aacf8016700b59942f9968bb72
Rohit Chawhan <40637586+Rohit5551998@users.noreply.github.com>
2021-06-19 15:22:58 +0000

    Strings

---
+++ Python Basics I/strings.py
@@ -0,0 +1,15 @@
+#Strings & String Concatenation
+print(type("hi hello there 24!"))
+username = 'supercoder'
+password = 'supersecret'
+long_string = '''
+WOW
+O O
+---
+'''
+
+print(long_string)
+first_name = 'Rohit'
+last_name = 'Chawhan'
+full_name = first_name + ' ' + last_name
+print(full_name)
