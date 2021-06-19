31bc3f17f9552b1d4779233c92eb887fcf11922b
Rohit Chawhan <40637586+Rohit5551998@users.noreply.github.com>
2021-06-19 15:40:39 +0000

    Formatted Strings

---
+++ Python Basics I/formatted_strings.py
@@ -0,0 +1,10 @@
+#Formatted Strings
+
+name = "Johnny"
+age = 55
+
+print(f'hi {name}. You are {age} years old')
+
+print("hi {}. You are {} years old".format(name, age))
+
+print('hi {1}. You are {0} years old'.format(name, age))
