fb5e0b76e7e94cfff51daba47f3a92ebaa70eacd
Rohit Chawhan <40637586+Rohit5551998@users.noreply.github.com>
2021-06-20 05:10:24 +0000

    Strings Built-In Methods

---
+++ Python Basics I/strings_builtin_methods.py
@@ -0,0 +1,12 @@
+#String Built-in Methods
+quote = 'to be or not to be'
+
+print(len(quote))
+print(quote.upper())
+print(quote.capitalize())
+print(quote.find('be'))
+print(quote.replace('be', 'me'))
+print(quote)
+
+quote2 = quote.replace('be', 'me')
+print(quote2)
