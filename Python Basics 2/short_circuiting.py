02c4e592110055e0f69df6469ed1c998f6ce98d3
Rohit Chawhan <40637586+Rohit5551998@users.noreply.github.com>
2021-06-21 07:38:21 +0000

    Exercise Logical Operators

---
+++ Python Basics 2/short_circuiting.py
@@ -0,0 +1,9 @@
+#Short Ciruiting
+is_friend = True
+is_user = True
+
+if False or is_user:
+	print("best friends forever")
+
+
+print('a' > '0' and 'a' > 'A')
---
+++ Python Basics 2/ternary_operator.py
@@ -0,0 +1,6 @@
+#Ternary Operator
+
+is_friend = True 
+can_message = "message allowed" if is_friend else "message not allowed"
+
+print(can_message)
