7aeee3a83f6a082054da916b7e518a0b01d8c140
Rohit Chawhan <40637586+Rohit5551998@users.noreply.github.com>
2021-08-10 07:25:02 +0000

    Generator and Performance

---
+++ Generators/Generator.py
@@ -0,0 +1,14 @@
+#Generator
+
+def generator_function(num):
+	for i in range(num):
+		yield i*2
+
+# for item in generator_function(10000):
+# 	print(item)
+
+g = generator_function(1000)
+next(g)
+next(g)
+print(next(g))
+print(next(g))
